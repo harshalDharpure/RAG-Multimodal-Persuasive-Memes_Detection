c263411b532666355e2a5e677c266a2e439719ac
Harshal Dharpure <63531290+harshalDharpure@users.noreply.github.com>
2024-05-17 22:33:35 +0530

    Update Task 3 comcat.py
    Update the Task 3 Code Without the MFB implementation
    
    Accuracy of the Model is 
    
    ──────────────────────────────────────────
           Test metric             DataLoader 0
    ────────────────────────────────────────────────────────────────────────────────────────────────────
            test_acc            0.6539999842643738
          test_f1_score         0.46001771092414856
            test_loss           0.6230312585830688
          test_roc_auc          0.5203425288200378
    ───────────────────────────────────────────────────

--- concat.py
+++ concat.py
@@ -2,10 +2,11 @@
 
     def __init__(self):
       super().__init__()
-      self.MFB = MFB(512,768,True,256,64,0.1)
+
+      self.MFB = MFB(512,768,True,192,64,0.1)
       self.fin_y_shape = torch.nn.Linear(768,512)
       self.fin_old = torch.nn.Linear(2048,2)
-      self.fin = torch.nn.Linear(16 * 768, 64)
+      self.fin_e = nn.Linear(16 * 768, 64)  # Adjusted input size to match the reshaped output from MFB
       self.fin_inten = torch.nn.Linear(2048,6)
       self.fin_e1 = torch.nn.Linear(2048,2)
       self.fin_e2 = torch.nn.Linear(2048,2)
@@ -16,47 +17,14 @@
       self.fin_e7 = torch.nn.Linear(2048,2)
       self.fin_e8 = torch.nn.Linear(2048,2)
       self.fin_e9 = torch.nn.Linear(2048,2)
-      # self.reduce_x = torch.nn.Linear(768, 512)
-      # self.reduce_rag = torch.nn.Linear(768, 512)
-
-
-
       self.validation_step_outputs = []
       self.test_step_outputs = []
 
 
     def forward(self, x,y,rag):
         x_,y_,rag_ = x,y,rag
-        print("x.shape", x.shape)
-        print("y.shape",y.shape)
-        # print("rag.shape",rag.shape)
-
-        # x = self.reduce_x(x)
-        # rag = self.reduce_rag(rag)
-
-        # print("x.shape", x.shape)
-        # print("y.shape",y.shape)
-        # print("rag.shape",rag.shape)
-        # z = self.MFB(torch.unsqueeze(y, axis=1), torch.unsqueeze(rag, axis=1))
-        # z_rag = self.MFB(torch.unsqueeze(y, axis=1),torch.unsqueeze(rag, axis=1))
-        # z_con = torch.cat((z, z_rag), dim=1)
-
-
-        # Concatenate x with y and then with rag
-
-
-        # z= torch.cat((torch.cat((x, y), dim=1), rag), dim=1)
-
-
-        # # Pass concatenated x with y and x with rag through your network
-        # z_new = torch.squeeze(z,dim=1)
-        # print("z_new shape",z_new)
-
-        z = torch.cat((x, y, rag), dim=1)
+        z= torch.cat((x, y, rag), dim=1)
         z_new = torch.squeeze(z, dim=1)
-
-
-
         c_inten = self.fin_inten(z_new)
         c_e1 = self.fin_e1(z_new)
         c_e2 = self.fin_e2(z_new)
@@ -69,22 +37,6 @@
         c_e9 = self.fin_e9(z_new)
         c = self.fin_old(z_new)
 
-        # print("z.shape",z.shape)
-        # print("z_new shape",z_new.shape)
-        # print("intensity error:", c_inten.shape)
-        # print("output:", c.shape)
-        # print("c_e1:", c_e1.shape)
-        # print("c_e2:", c_e2.shape)
-        # print("c_e3:", c_e3.shape)
-        # print("c_e4:", c_e4.shape)
-        # print("c_e5:", c_e5.shape)
-        # print("c_e6:", c_e6.shape)
-        # print("c_e7:", c_e7.shape)
-        # print("c_e8:", c_e8.shape)
-        # print("c_e9:", c_e9.shape)
-        # print("logits.shape",logits.shape)
-
-
         output = torch.log_softmax(c, dim=1)
         c_inten = torch.log_softmax(c_inten, dim=1)
         c_e1 = torch.log_softmax(c_e1, dim=1)
@@ -96,65 +48,30 @@
         c_e7 = torch.log_softmax(c_e7, dim=1)
         c_e8 = torch.log_softmax(c_e8, dim=1)
         c_e9 = torch.log_softmax(c_e9, dim=1)
-
         return output,c_inten,c_e1,c_e2,c_e3,c_e4,c_e5,c_e6,c_e7,c_e8,c_e9
 
 
     def cross_entropy_loss(self, logits, labels):
-
-        return F.nll_loss(logits, labels)
+      return F.nll_loss(logits, labels)
 
     def training_step(self, train_batch, batch_idx):
-        #lab,txt,rag,img,name,per,iro,alli,ana,inv,meta,puns,sat,hyp= train_batch
         lab,txt,rag,img,name,intensity,e1,e2,e3,e4,e5,e6,e7,e8,e9= train_batch
-        #logit_offen,a,b,c,d,e,f,g,h,i,logit_inten_target= self.forward(txt,img,rag)
-
-        lab = train_batch[lab].unsqueeze(1)
-        #print(lab)
+        lab = train_batch[lab]   
         txt = train_batch[txt]
         rag = train_batch[rag]
         img = train_batch[img]
         name= train_batch[name]
-        intensity = train_batch[intensity].unsqueeze(1)
-        e1 = train_batch[e1].unsqueeze(1)
-        e2 = train_batch[e2].unsqueeze(1)
-        e3 = train_batch[e3].unsqueeze(1)
-        e4 = train_batch[e4].unsqueeze(1)
-        e5 = train_batch[e5].unsqueeze(1)
-        e6 = train_batch[e6].unsqueeze(1)
-        e7 = train_batch[e7].unsqueeze(1)
-        e8 = train_batch[e8].unsqueeze(1)
-        e9 = train_batch[e9].unsqueeze(1)
-
-        lab = F.one_hot(lab, num_classes=2)
-        intensity = torch.abs(intensity)
-        intensity = F.one_hot(intensity, num_classes=6)  # Assuming you have 6 classes
-        e1 = F.one_hot(e1,num_classes = 2)
-        e2 = F.one_hot(e2,num_classes = 2)
-        e3 = F.one_hot(e3,num_classes = 2)
-        e4 = F.one_hot(e4,num_classes = 2)
-        e5 = F.one_hot(e5,num_classes = 2)
-        e6 = F.one_hot(e6,num_classes = 2)
-        e7 = F.one_hot(e7,num_classes = 2)
-        e8 = F.one_hot(e8,num_classes = 2)
-        e9 = F.one_hot(e9,num_classes = 2)
-
-        lab = lab.squeeze(dim=1)
-        intensity = intensity.squeeze(dim=1)
-        e1 = e1.squeeze(dim=1)
-        e2 = e2.squeeze(dim=1)
-        e3 = e3.squeeze(dim=1)
-        e4 = e4.squeeze(dim=1)
-        e5 = e5.squeeze(dim=1)
-        e6 = e6.squeeze(dim=1)
-        e7 = e7.squeeze(dim=1)
-        e8 = e8.squeeze(dim=1)
-        e9 = e9.squeeze(dim=1)
-
-
-
+        intensity = train_batch[intensity]
+        e1 = train_batch[e1]
+        e2 = train_batch[e2]
+        e3 = train_batch[e3]
+        e4 = train_batch[e4]
+        e5 = train_batch[e5]
+        e6 = train_batch[e6]
+        e7 = train_batch[e7]
+        e8 = train_batch[e8]
+        e9 = train_batch[e9]
         logit_offen,logit_inten_target,a,b,c,d,e,f,g,h,i= self.forward(txt,img,rag)
-
         loss1 = self.cross_entropy_loss(logit_offen, lab)
         loss17 = self.cross_entropy_loss(logit_inten_target, intensity)
         loss4 = self.cross_entropy_loss(a, e1)
@@ -166,69 +83,35 @@
         loss10 = self.cross_entropy_loss(g, e7)
         loss11 = self.cross_entropy_loss(h, e8)
         loss12 = self.cross_entropy_loss(i, e9)
-
         loss = loss1 + loss4 + loss5 + loss6 + loss7 + loss8 +loss9 + loss10 +loss11 +loss12 + loss17
-
         self.log('train_loss', loss)
         return loss
 
 
     def validation_step(self, val_batch, batch_idx):
-        #lab,txt,rag,img,name,per,iro,alli,ana,inv,meta,puns,sat,hyp = val_batch
         lab,txt,rag,img,name,intensity,e1,e2,e3,e4,e5,e6,e7,e8,e9= val_batch
-        lab = val_batch[lab].unsqueeze(1)
-        #print(lab)
+        lab = val_batch[lab]
         txt = val_batch[txt]
         rag = val_batch[rag]
         img = val_batch[img]
         name = val_batch[name]
-        intensity = val_batch[intensity].unsqueeze(1)
-        e1 = val_batch[e1].unsqueeze(1)
-        e2 = val_batch[e2].unsqueeze(1)
-        e3 = val_batch[e3].unsqueeze(1)
-        e4 = val_batch[e4].unsqueeze(1)
-        e5 = val_batch[e5].unsqueeze(1)
-        e6 = val_batch[e6].unsqueeze(1)
-        e7 = val_batch[e7].unsqueeze(1)
-        e8 = val_batch[e8].unsqueeze(1)
-        e9 = val_batch[e9].unsqueeze(1)
-
-        lab = F.one_hot(lab, num_classes=2)
-
-        intensity = torch.abs(intensity)
-        intensity = F.one_hot(intensity, num_classes=6)
-        e1 = F.one_hot(e1,num_classes = 2)
-        e2 = F.one_hot(e2,num_classes = 2)
-        e3 = F.one_hot(e3,num_classes = 2)
-        e4 = F.one_hot(e4,num_classes = 2)
-        e5 = F.one_hot(e5,num_classes = 2)
-        e6 = F.one_hot(e6,num_classes = 2)
-        e7 = F.one_hot(e7,num_classes = 2)
-        e8 = F.one_hot(e8,num_classes = 2)
-        e9 = F.one_hot(e9,num_classes = 2)
-        lab = lab.squeeze(dim=1)
-
-
-        intensity = intensity.squeeze(dim = 1)
-        e1 = e1.squeeze(dim=1)
-        e2 = e2.squeeze(dim=1)
-        e3 = e3.squeeze(dim=1)
-        e4 = e4.squeeze(dim=1)
-        e5 = e5.squeeze(dim=1)
-        e6 = e6.squeeze(dim=1)
-        e7 = e7.squeeze(dim=1)
-        e8 = e8.squeeze(dim=1)
-        e9 = e9.squeeze(dim=1)
-
+        intensity = val_batch[intensity]
+        e1 = val_batch[e1]
+        e2 = val_batch[e2]
+        e3 = val_batch[e3]
+        e4 = val_batch[e4]
+        e5 = val_batch[e5]
+        e6 = val_batch[e6]
+        e7 = val_batch[e7]
+        e8 = val_batch[e8]
+        e9 = val_batch[e9]
         logits,inten,a,b,c,d,e,f,g,h,i = self.forward(txt,img,rag)
-
         logits=logits.float()
-
         tmp = np.argmax(logits.detach().cpu().numpy(),axis=1)
         loss = self.cross_entropy_loss(logits, lab)
         lab = lab.detach().cpu().numpy()
         self.log('val_acc', accuracy_score(lab,tmp))
-        self.log('val_roc_auc',roc_auc_score(lab,tmp))
+        # self.log('val_roc_auc',roc_auc_score(lab,tmp))
         self.log('val_loss', loss)
         tqdm_dict = {'val_acc': accuracy_score(lab,tmp)}
         self.validation_step_outputs.append({'progress_bar': tqdm_dict,'val_f1 offensive': f1_score(lab,tmp,average='macro')})
@@ -252,47 +135,23 @@
 
     def test_step(self, batch, batch_idx):
         lab,txt,rag,img,name,intensity,e1,e2,e3,e4,e5,e6,e7,e8,e9= batch
-        lab = batch[lab].unsqueeze(1)
+        lab = batch[lab]
         #print(lab)
         txt = batch[txt]
         rag = batch[rag]
         img = batch[img]
         name = batch[name]
-        intensity = batch[intensity].unsqueeze(1)
-        e1 = batch[e1].unsqueeze(1)
-        e2 = batch[e2].unsqueeze(1)
-        e3 = batch[e3].unsqueeze(1)
-        e4 = batch[e4].unsqueeze(1)
-        e5 = batch[e5].unsqueeze(1)
-        e6 = batch[e6].unsqueeze(1)
-        e7 = batch[e7].unsqueeze(1)
-        e8 = batch[e8].unsqueeze(1)
-        e9 = batch[e9].unsqueeze(1)
-        lab = F.one_hot(lab, num_classes=2)
-        intensity = F.one_hot(intensity, num_classes=6)
-        e1 = F.one_hot(e1,num_classes = 2)
-        e2 = F.one_hot(e2,num_classes = 2)
-        e3 = F.one_hot(e3,num_classes = 2)
-        e4 = F.one_hot(e4,num_classes = 2)
-        e5 = F.one_hot(e5,num_classes = 2)
-        e6 = F.one_hot(e6,num_classes = 2)
-        e7 = F.one_hot(e7,num_classes = 2)
-        e8 = F.one_hot(e8,num_classes = 2)
-        e9 = F.one_hot(e9,num_classes = 2)
-        lab = lab.squeeze(dim=1)
-        intensity = intensity.squeeze(dim=1)
-        e1 = e1.squeeze(dim=1)
-        e2 = e2.squeeze(dim=1)
-        e3 = e3.squeeze(dim=1)
-        e4 = e4.squeeze(dim=1)
-        e5 = e5.squeeze(dim=1)
-        e6 = e6.squeeze(dim=1)
-        e7 = e7.squeeze(dim=1)
-        e8 = e8.squeeze(dim=1)
-        e9 = e9.squeeze(dim=1)
-
+        intensity = batch[intensity]
+        e1 = batch[e1]
+        e2 = batch[e2]
+        e3 = batch[e3]
+        e4 = batch[e4]
+        e5 = batch[e5]
+        e6 = batch[e6]
+        e7 = batch[e7]
+        e8 = batch[e8]
+        e9 = batch[e9]
         logits,inten,a,b,c,d,e,f,g,h,i= self.forward(txt,img,rag)
-
         logits = logits.float()
         tmp = np.argmax(logits.detach().cpu().numpy(force=True),axis=-1)
         loss = self.cross_entropy_loss(logits, lab)
@@ -320,8 +179,7 @@
         self.test_step_outputs.clear()
 
     def configure_optimizers(self):
-      # optimizer = torch.optim.Adam(self.parameters(), lr=3e-2)
-      optimizer = torch.optim.Adam(self.parameters(), lr=1e-5)
+      optimizer = torch.optim.Adam(self.parameters(), lr=1e-6)
 
       return optimizer
 
@@ -346,13 +204,13 @@
       self.hm_test = te_p
 
     def train_dataloader(self):
-      return DataLoader(self.hm_train, batch_size=10, drop_last=True)
+      return DataLoader(self.hm_train, batch_size=8, drop_last=True)
 
     def val_dataloader(self):
-      return DataLoader(self.hm_val, batch_size=10, drop_last=True)
+      return DataLoader(self.hm_val, batch_size=8, drop_last=True)
 
     def test_dataloader(self):
-      return DataLoader(self.hm_test, batch_size=10, drop_last=True)
+      return DataLoader(self.hm_test, batch_size=8, drop_last=True)
 
   data_module = HmDataModule()
   checkpoint_callback = ModelCheckpoint(
